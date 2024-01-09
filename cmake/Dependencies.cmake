8002ab4e2d4169374e3669e5135964471b1c94b5
Yuri Witte <witte@tuta.io>
2024-01-09 17:02:00 -0300

    use clap-helpers cmake target

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,27 +1,19 @@
-find_package(clap CONFIG)
+include(FetchContent)
 
-if (NOT clap_FOUND)
-    message("Using FetchContent to donwload 'clap' and 'clap-helpers'")
+FetchContent_Declare(
+    clap
+    GIT_REPOSITORY https://github.com/free-audio/clap.git
+    GIT_TAG main
+    # 'FIND_PACKAGE_ARGS' will skip download if
+    # the target is already available in the system
+    FIND_PACKAGE_ARGS NAMES clap
+)
 
-    include(FetchContent)
-    FetchContent_Declare(
-        clap
-        GIT_REPOSITORY https://github.com/free-audio/clap.git
-        GIT_TAG main
-        FIND_PACKAGE_ARGS NAMES clap
-    )
+FetchContent_Declare(
+    clap-helpers
+    GIT_REPOSITORY https://github.com/free-audio/clap-helpers.git
+    GIT_TAG main
+    FIND_PACKAGE_ARGS NAMES clap-helpers
+)
 
-    # Installed clap-helpers doesn't have a cmake target, so for now
-    # I'm treating 'clap' and 'clap-helpers' as the same thing
-    FetchContent_Declare(
-        clap-helpers
-        GIT_REPOSITORY https://github.com/free-audio/clap-helpers.git
-        GIT_TAG main
-        FIND_PACKAGE_ARGS NAMES clap-helpers
-    )
-
-    # Remove this whenever clap-helpers has a cmake target
-    include_directories(SYSTEM ${FETCHCONTENT_BASE_DIR}/clap-helpers-src/include)
-
-    FetchContent_MakeAvailable(clap clap-helpers)
-endif()
+FetchContent_MakeAvailable(clap clap-helpers)
